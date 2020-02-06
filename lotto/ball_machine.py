99148606d8fb1c66b27c2ecc1ce4d31fadaf1a01
Dlean <DleanJeans@gmail.com>
2020-02-06 11:22:04 +0700

    ball_machine: Use timestamp as seed

--- lotto/ball_machine.py
+++ lotto/ball_machine.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime
 from lotto.draw_result import DrawResult
 
 DRAW_SEQUENCE = list(range(7))
@@ -6,6 +7,7 @@ DRAW_SEQUENCE[0] = 1
 TOTAL_BALLS = sum(DRAW_SEQUENCE)
 
 def draw():
+    random.seed(datetime.now())
     pool = list(range(100))
     winning_balls = random.sample(pool, TOTAL_BALLS)
     return DrawResult(winning_balls)
